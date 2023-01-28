9d39e62fd067d889202a0230fec59310c1dbf1a1
kijoupa <kijoupa@hotmail.com>
2023-01-28 20:43:13 +0100

    best changes

---
+++ cgpt.py
@@ -0,0 +1,18 @@
+﻿from difflib import HtmlDiff
+
+# original strings
+string1="Hello World2"
+string2="Hello Hello World"
+
+# create a diff object
+diff = HtmlDiff()
+
+# compare the strings
+diff_result = diff.make_file(string1.splitlines(), string2.splitlines())
+
+# print the differences
+print(diff_result)
+
+file = open("cgpt.html", "w")
+file.write(diff_result)
+file.close()
